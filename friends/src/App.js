06b821a21a6b7520afb15ef2e02c34582a2a371e
Lakendria <kendra939@gmail.com>
2021-01-17 19:47:50 -0500

    Login Form Created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
+  const logout = () =>{
+    ;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <Router>
+      <ul>
+        <li>
+          <Link to ="/login">Login</Link>
+        </li>
+        <li>
+        <Link onClick={logout}>Logout</Link>
+        </li>
+        <li>
+          <Link to ="/protected">Protected Page</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path = "/protected" component = {FriendsList}/>
+        <Route path = "/login" component={Login}/>
+        <Route component = {Login}/>
+      </Switch>
+</Router>
     </div>
   );
 }
